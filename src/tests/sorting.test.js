1047e2cd34df2c026d8543ea01ba5741e0466b90
XNAK3 <yagami01018@gmail.com>
2026-07-08 14:08:47 -0400

    Refactor code to readibility

--- src/tests/sorting.test.js
+++ src/tests/sorting.test.js
@@ -3,23 +3,8 @@ import {
   insertionSort,
   selectionSort,
 } from "../algorithms/sortFns.js";
+import tests from '../samples.js'
 
-const tests = [
-  {
-    input: [5, 4, 3, 2, 1, 44, 0, -1, 1, 9, 6],
-    output: [-1, 0, 1, 1, 2, 3, 4, 5, 6, 9, 44],
-  },
-  { input: [1, 2, 3, 4, 5], output: [1, 2, 3, 4, 5] },
-  {
-    input: [10, 9, 7, 6, 5],
-    output: [5, 6, 7, 9, 10],
-  },
-  {
-    input: [],
-    output: [],
-  },
-  { input: [1], output: [1] },
-];
 
 describe("BubbleSort", () => {
   tests.forEach(({ input, output }) => {
