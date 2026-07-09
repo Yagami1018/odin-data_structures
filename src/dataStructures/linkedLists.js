cb8cec0a2a9f80cc50029913a008eb2e525b7cf2
XNAK3 <yagami01018@gmail.com>
2026-07-09 04:14:57 -0400

    Implement prepend at linkedLists

--- src/dataStructures/linkedLists.js
+++ src/dataStructures/linkedLists.js
@@ -2,41 +2,47 @@ class LinkedList {
   constructor(...arr) {
     this.head = null;
     this.tail = null;
-    this.size = 0
+    this.size = 0;
     for (let val of arr) {
       this.append(val);
     }
-}
+  }
 
+  prepend(value) {
+    const newNode = new Node(value, this.head);
+    if (!this.head) this.tail = newNode;
+    this.head = newNode;
+    this.size++;
+  }
 
-append(value) {
+  append(value) {
     const newNode = new Node(value, null);
     if (!this.head) {
-        this.head = newNode;
-        this.tail = newNode;
+      this.head = newNode;
+      this.tail = newNode;
     } else {
-        this.tail.next = newNode;
-        this.tail = newNode;
+      this.tail.next = newNode;
+      this.tail = newNode;
     }
-    this.size++
+    this.size++;
   }
 
   at(index) {
-    if(index>=this.size || index < 0) throw new RangeError(`Error: Index ${index} out of bounds (size: ${this.size})`)
-    let current = this.head
-    for(let i = 0; i< index; i++) {
-        current = current.next
+    if (index >= this.size || index < 0) return;
+    let current = this.head;
+    for (let i = 0; i < index; i++) {
+      current = current.next;
     }
-    return current.value
+    return current.value;
   }
 
   toString() {
     let str = "";
-    let current = this.head
-    while(current) {
-        str+= current.value
-        if(current.next) str += ' -> '
-        current = current.next
+    let current = this.head;
+    while (current) {
+      str += current.value;
+      if (current.next) str += " -> ";
+      current = current.next;
     }
     return str;
   }
@@ -51,7 +57,11 @@ class Node {
   }
 }
 
-let list = new LinkedList(1, 2, 3, 4, 5);
+let list = new LinkedList();
+list.append(1)
 
 console.log(list.toString());
-console.log(list.at(1))
+console.log(list.at(0));
+
+list.prepend(6);
+console.log(list.toString());
