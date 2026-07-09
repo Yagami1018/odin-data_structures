a60900f7bc39d3595d77b43a567d5c0d7801943a
XNAK3 <yagami01018@gmail.com>
2026-07-09 03:57:47 -0400

    Implement append, at, toString methos at linkedList class

---
+++ src/dataStructures/linkedLists.js
@@ -0,0 +1,57 @@
+class LinkedList {
+  constructor(...arr) {
+    this.head = null;
+    this.tail = null;
+    this.size = 0
+    for (let val of arr) {
+      this.append(val);
+    }
+}
+
+
+append(value) {
+    const newNode = new Node(value, null);
+    if (!this.head) {
+        this.head = newNode;
+        this.tail = newNode;
+    } else {
+        this.tail.next = newNode;
+        this.tail = newNode;
+    }
+    this.size++
+  }
+
+  at(index) {
+    if(index>=this.size || index < 0) throw new RangeError(`Error: Index ${index} out of bounds (size: ${this.size})`)
+    let current = this.head
+    for(let i = 0; i< index; i++) {
+        current = current.next
+    }
+    return current.value
+  }
+
+  toString() {
+    let str = "";
+    let current = this.head
+    while(current) {
+        str+= current.value
+        if(current.next) str += ' -> '
+        current = current.next
+    }
+    return str;
+  }
+}
+
+class Node {
+  value = null;
+  next = null;
+  constructor(value, next) {
+    this.value = value;
+    this.next = next;
+  }
+}
+
+let list = new LinkedList(1, 2, 3, 4, 5);
+
+console.log(list.toString());
+console.log(list.at(1))
