38a138709ce853453b60d7fc460cc4abfd2864f4
XNAK3 <yagami01018@gmail.com>
2026-07-12 03:36:31 -0400

    Setup constructor

--- src/dataStructures/hashmap.js
+++ src/dataStructures/hashmap.js
@@ -1,10 +1,14 @@
 class HashMap {
-    hash(key) {
+    constructor() {
+       this.buckets = new Array(16).fill(null).map(()=>[])
+       this.size = 0 
+    }
+    _hash(key) {
         let hashcode = 0;
         const primeNumber=31;
 
-        for(let i = 0; i<key; i++) {
-            hashcode += primeNumber * hashcode + key.charCodeAt(i);
+        for(let i = 0; i<key.length; i++) {
+            hashcode = (primeNumber * hashcode + key.charCodeAt(i)) % this.buckets.length;
         }
         return hashcode;
     }
