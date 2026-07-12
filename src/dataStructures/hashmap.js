0145e110a48c2203d44fe7047cd560ad7fabf6bf
XNAK3 <yagami01018@gmail.com>
2026-07-12 03:40:51 -0400

    Implement set(key,value) method

--- src/dataStructures/hashmap.js
+++ src/dataStructures/hashmap.js
@@ -1,15 +1,31 @@
 class HashMap {
-    constructor() {
-       this.buckets = new Array(16).fill(null).map(()=>[])
-       this.size = 0 
-    }
-    _hash(key) {
-        let hashcode = 0;
-        const primeNumber=31;
+  constructor() {
+    this.buckets = new Array(16).fill(null).map(() => []);
+    this.size = 0;
+  }
 
-        for(let i = 0; i<key.length; i++) {
-            hashcode = (primeNumber * hashcode + key.charCodeAt(i)) % this.buckets.length;
-        }
-        return hashcode;
+  _hash(key) {
+    let hashcode = 0;
+    const primeNumber = 31;
+
+    for (let i = 0; i < key.length; i++) {
+      hashcode =
+        (primeNumber * hashcode + key.charCodeAt(i)) % this.buckets.length;
     }
+    return hashcode;
+  }
+
+  set(key, value) {
+    const index = this._hash(key);
+    const bucket = this.buckets[index];
+
+    for (let pair of bucket) {
+      if (pair[0] === key) {
+        pair[1] = value;
+        return;
+      }
+      bucket.push([key,value])
+      this.size++
+    }
+  }
 }
