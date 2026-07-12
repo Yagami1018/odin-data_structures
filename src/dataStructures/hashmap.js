a6a89e6c3e7fe43f5b3e875e20717e5fa4a95fd0
XNAK3 <yagami01018@gmail.com>
2026-07-12 12:17:28 -0400

    Format using prettier

--- src/dataStructures/hashmap.js
+++ src/dataStructures/hashmap.js
@@ -1,31 +1,30 @@
 class HashMap {
-  constructor() {
-    this.buckets = new Array(16).fill(null).map(() => []);
-    this.size = 0;
-  }
-
-  _hash(key) {
-    let hashcode = 0;
-    const primeNumber = 31;
-
-    for (let i = 0; i < key.length; i++) {
-      hashcode =
-        (primeNumber * hashcode + key.charCodeAt(i)) % this.buckets.length;
+    constructor() {
+        this.buckets = new Array(16).fill(null).map(() => []);
+        this.size = 0;
     }
-    return hashcode;
-  }
 
-  set(key, value) {
-    const index = this._hash(key);
-    const bucket = this.buckets[index];
+    _hash(key) {
+        let hashcode = 0;
+        const primeNumber = 31;
 
-    for (let pair of bucket) {
-      if (pair[0] === key) {
-        pair[1] = value;
-        return;
-      }
-      bucket.push([key,value])
-      this.size++
+        for (let i = 0; i < key.length; i++) {
+            hashcode = (primeNumber * hashcode + key.charCodeAt(i)) % this.buckets.length;
+        }
+        return hashcode;
     }
-  }
+
+    set(key, value) {
+        const index = this._hash(key);
+        const bucket = this.buckets[index];
+
+        for (let pair of bucket) {
+            if (pair[0] === key) {
+                pair[1] = value;
+                return;
+            }
+            bucket.push([key, value]);
+            this.size++;
+        }
+    }
 }
