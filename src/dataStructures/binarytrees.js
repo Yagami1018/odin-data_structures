de390d57717819844749ee0fcfe893e891843f04
XNAK3 <yagami01018@gmail.com>
2026-07-13 05:55:30 -0400

    Refactor

--- src/dataStructures/binarytrees.js
+++ src/dataStructures/binarytrees.js
@@ -1,14 +1,14 @@
 export class BinarySearchTree {
     constructor(arr = []) {
-        let sorted = arr.toSorted((a, b) => a - b);
-        sorted = Array.from(new Set(sorted));
+        const sorted = [...new Set(arr.toSorted((a, b) => a - b))];
         this.root = this._buildTree(sorted, 0, sorted.length - 1);
     }
-    _buildTree(arr = [], start, end) {
-        if (start > end) return null;
-        let middle = start + Math.floor((end - start) / 2);
 
-        let root = new Node(arr[middle]);
+    _buildTree(arr, start, end) {
+        if (start > end) return null;
+        const middle = start + Math.floor((end - start) / 2);
+
+        const root = new Node(arr[middle]);
         root.left = this._buildTree(arr, start, middle - 1);
         root.right = this._buildTree(arr, middle + 1, end);
 
